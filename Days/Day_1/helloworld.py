b4bd8b173bb1ed310257c3b90f19d988e490201f
Manny Lee <80362648+many221@users.noreply.github.com>
2022-07-02 23:01:10 -0700

    Initial commit

---
+++ Days/Day_1/helloworld.py
@@ -0,0 +1,35 @@
+
+# Exercise 1
+print(2+3)
+print(2-3)
+print(2/3)
+print(2*3)
+print(2%3)
+print(2**3)
+print(2//3)
+
+# Exercise 2
+first_name = "Manny"
+last_name = "Lee"
+country = "USA"
+text = "Working on Day 1 of 30 Days of Python"
+print(first_name)
+print(last_name)
+print(country)
+print(text)
+
+# Exercise 3
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(first_name))
+print(type(last_name))
+print(type(country))
+
+# Exercise 4
+# calculating euclidean distance between vectors 2, 3 and 10, 8
+# Formula d =√[(x2 – x1)2 + (y2 – y1)2]
+from math import sqrt
+print(sqrt(abs((10-2)**2+(8-3)**2)))
