df68e990a46c1e9407145c96987bd88930769028
Manny Lee <many221@gmail.com>
2022-07-03 23:57:55 -0700

    Day 2

---
+++ Days/Day_2/variables.py
@@ -0,0 +1,43 @@
+# Day 2: 30 Days of python programming
+
+# Exercise 1
+
+first_name = "Manny"
+last_name = "Lee"
+full_name = "Manny Lee"
+country = "USA"
+city = "San Francisco"
+age = 25
+year = 2022
+is_true = True
+is_light_on = False
+
+height, weight, hair_color = 5.8, 160, "Black"
+
+
+# Exercise 2
+
+print(type(first_name))
+print(type(last_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(full_name))
+print(len(first_name) > len(last_name))
+
+sum_one = 5
+sum_two = 4
+total = sum_one + sum_two
+diff = sum_one - sum_two
+prod = sum_one * sum_two
+division = sum_one / sum_two
+remainder = sum_one % sum_two
+exp = sum_one ** sum_two
+floor = sum_one // sum_two
+
+print(total,diff,prod,division,remainder,exp,floor)
+
